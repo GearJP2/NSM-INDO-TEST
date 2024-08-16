f8e96d35b9b12b69c2a52d134f617bbff479d1c4
GearJP2 <157605301+GearJP2@users.noreply.github.com>
2024-08-17 01:23:28 +0700

    Update app.py

--- onstreamlit-test/streamlit-audio-recorder-main/app.py
+++ onstreamlit-test/streamlit-audio-recorder-main/app.py
@@ -9,6 +9,7 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
 import time
+import io  # Added import
 import os
 
 # Authenticate and create the PyDrive client
@@ -18,7 +19,7 @@ def create_drive_client():
     scope = ['https://www.googleapis.com/auth/drive']
     
     # Authenticate using service account credentials
-    creds = ServiceAccountCredentials.from_json_keyfile_name('onstreamlit-test/streamlit-audio-recorder-main/heart-d9410-9a288317e3c7.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/heart-d9410-9a288317e3c7.json', scope)
     gauth = GoogleAuth()
     gauth.credentials = creds
     drive = GoogleDrive(gauth)
